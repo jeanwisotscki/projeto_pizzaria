3100e307b76f9753e3267871f3b274ff5a649a86
Jean Wisotscki <jean1wisotscki@gmail.com>
2021-06-20 12:10:38 -0300

    tela de compra dos itens iniciada

--- script.js
+++ script.js
@@ -4,11 +4,22 @@ const qS = (elemento)=> document.querySelectorAll(elemento) // função anônima
 pizzaJson.map((item, index)=>{
     let pizzaItem = q('.models .pizza-item').cloneNode(true)
 
-    // preenchimento das informações em pizzaItem
+    // preenchimento da listagem dos itens
     pizzaItem.querySelector('.pizza-item--img img').src = item.img
     pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`
     pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name
     pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description
+
+    // tela de compra dos itens
+    pizzaItem.querySelector('a').addEventListener('click', (evento)=>{
+        evento.preventDefault()
+
+        q('.pizzaWindowArea').style.opacity = 0
+        q('.pizzaWindowArea').style.display = 'flex'
+        setTimeout(()=>{
+            q('.pizzaWindowArea').style.opacity = 1
+        }, 200)
+    })
 
     q('.pizza-area').append(pizzaItem)
 })
